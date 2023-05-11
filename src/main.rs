2c495f9bec9b543f505dd8005adecb46cfc199d6
Paulo Lins <plins@ebi.ac.uk>
2023-05-11 22:11:34 +0100

    remove app call

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,14 @@ pub mod security;
 pub mod serializers;
 pub mod utils;
 
+use clap::Parser;
+
 fn main() {
-    cli::app();
+    let cli = cli::Cli::parse();
+    match cli::match_command(&cli) {
+        Ok(_) => (),
+        Err(error) => {
+            println!("{:?}", error)
+        }
+    }
 }
